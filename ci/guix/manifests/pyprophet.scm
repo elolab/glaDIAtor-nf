e10f3fa00677ea8472f54364bedc428a8dc64c4a
Mats <>
2023-02-17 13:40:25 +0200

    add non-legacy pyprophet package definition
    * pyprophet.scm (pyprophet):
    (python-pyprophet):

---
+++ ci/guix/manifests/pyprophet.scm
@@ -0,0 +1,53 @@
+(define-module (pyprophet)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system python)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages machine-learning)
+  #:use-module (gnu packages python-science)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (guix build utils)
+  #:use-module (guix profiles)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages pdf)
+  #:use-module (gnu packages statistics)
+  #:use-module ((guix licenses) #:prefix license:)
+   )
+
+
+(define-public python-pyprophet
+  (package
+    (name "python-pyprophet")
+    (version "2.2.3")
+    (source (origin
+              (method url-fetch)
+              (uri (pypi-uri "pyprophet" version))
+              (sha256
+               (base32
+                "1cbw25zdmlagnir21p7yd0fqcdcvy9w8qs9sdcbvwk3y0qqrlh3k"))))
+    (build-system python-build-system)
+    (propagated-inputs (list python-click
+                             python-cython
+                             python-hyperopt
+                             python-matplotlib
+                             python-numexpr
+                             python-numpy
+                             python-pandas
+                             python-pypdf2
+                             python-scikit-learn
+                             python-scipy
+                             python-statsmodels
+                             python-tabulate
+                             python-xgboost))
+    (home-page "https://github.com/PyProphet/pyprophet")
+    (synopsis
+     "PyProphet: Semi-supervised learning and scoring of OpenSWATH results.")
+    (description
+     "PyProphet: Semi-supervised learning and scoring of OpenSWATH results.")
+    (license license:bsd-3)))
+python-pyprophet
+
+;; Local Variables:
+;; compile-command: "guix time-machine -C ../channels.scm -- build -f pyprophet.scm"
+;; End:
