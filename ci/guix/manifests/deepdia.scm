627eb9eb51c686a7d9dab0411cae932fd0d80e69
Mats <>
2023-05-19 18:15:41 +0300

    work around python-pydot's pyparsing pinning
    * deepdia.scm ("python-deepdia"):
      python-pydot requries pyparsing@2.4.7
      which is a conflict in profile.
      So we set it python-pydot to not use tests,
      and we make python-pyparsing@2.4.7 point to python-pyparsing.

--- ci/guix/manifests/deepdia.scm
+++ ci/guix/manifests/deepdia.scm
@@ -1,7 +1,18 @@
 ;; manifest for usage together with the emacs manifest
 ;; to generate the html documentation
-(specifications->manifest
+(use-modules (guix transformations))
+(packages->manifest
+ (map
+  (compose
+   ;; https://github.com/lkk7/pydot/tree/pyparsing_fix
+   ;; python-pydot has python-pyparsing@2.4.7 requirement 2.4.7, others require 3.0.6
+   ;; we're not gonna do graphviz things here, so it might be okay to have a broken python-pydot here?
+   (options->transformation
+    `((with-input . "python-pyparsing@2.4.7=python-pyparsing")
+      (without-tests . "python-pydot")))
+   specification->package)
+ 
  '("python-deepdia"
    ;; because nextflow requires bash to be present
-   "bash" ))
+   "bash" )))
     
